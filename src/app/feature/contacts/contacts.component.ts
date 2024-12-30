6915315ff61e0df133e614f985aec79de1eebe15
sap <sap@cca.cz>
2024-12-30 17:16:42 +0100

    contact form init impl, errors console fixed

--- src/app/feature/contacts/contacts.component.ts
+++ src/app/feature/contacts/contacts.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ContactFormComponent} from "../contact-form/contact-form.component";
 
 @Component({
   selector: 'app-contacts',
   standalone: true,
-  imports: [],
+  imports: [
+    ContactFormComponent
+  ],
   templateUrl: './contacts.component.html',
   styleUrl: './contacts.component.scss'
 })
