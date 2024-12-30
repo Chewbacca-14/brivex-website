728e52616da28d9b8a058cf5209d505b91a6dec2
Max <bulanovich5@seznam.cz>
2024-12-30 22:26:52 +0100

    added about us

--- src/app/shared/components/about-us-card/about-us-card.component.ts
+++ src/app/shared/components/about-us-card/about-us-card.component.ts
@@ -1,13 +1,32 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactBubbleComponent } from '../contact-bubble/contact-bubble.component';
 
 @Component({
   selector: 'app-about-us-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ContactBubbleComponent],
   templateUrl: './about-us-card.component.html',
   styleUrls: ['./about-us-card.component.scss'],
 })
 export class AboutUsCardComponent {
   @Input() backgroundImage: string = '';
+  @Input() avatar: string = '';
+  @Input() nameSurname: string = '';
+  @Input() position: string = '';
+
+  contacts = [
+    {
+      icon: '/assets/svg/message-icon.svg',
+      bgColor: 'white',
+    },
+    {
+      icon: '/assets/svg/linkedin-icon.svg',
+      bgColor: 'white',
+    },
+    {
+      icon: '/assets/svg/email-icon.svg',
+      bgColor: 'white',
+    },
+  ];
 }
