cb86e7bb9232af6ba2f4c01751e8aecd4e2699ae
sap <sap@cca.cz>
2025-01-02 12:45:41 +0100

    scss responsiveness added for medium monitor, bug fixed: slowed margins for ARM removed

--- src/app/shared/components/stepper/stepper.component.ts
+++ src/app/shared/components/stepper/stepper.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { MatStepperModule } from '@angular/material/stepper';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {MatStepper, MatStepperModule} from '@angular/material/stepper';
 import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 
@@ -10,4 +10,29 @@ import { CommonModule } from '@angular/common';
   templateUrl: './stepper.component.html',
   styleUrls: ['./stepper.component.scss'],
 })
-export class StepperComponent {}
+export class StepperComponent implements AfterViewInit{
+  steps = [
+    { label: 'Konzultace a analýza', content: 'Každý projekt je pro nás příběhem, který tvoříme krok za krokem...' },
+    { label: 'Návrh řešení', content: 'Navrhneme řešení na míru vašim potřebám a možnostem.' },
+    { label: 'Realizace projektu', content: 'Přeneseme návrh do reality pomocí moderních technologií.' },
+    { label: 'Testování a ladění', content: 'Pečlivě testujeme každý krok, aby vše fungovalo bezchybně.' },
+    { label: 'Dodání a podpora', content: 'Dokončený projekt předáme a zajišťujeme jeho další podporu.' },
+  ];
+
+  ngAfterViewInit(): void {
+    const mouseTarget = document.querySelector(".matStepId") as HTMLElement;
+
+    mouseTarget.addEventListener("mouseenter", (e) => {
+      mouseTarget.style.color = "green";
+    });
+
+    mouseTarget.addEventListener("mouseleave", (e) => {
+      mouseTarget.style.color = "black";
+    });
+  }
+
+  onStepHover(stepper: MatStepper, index: number): void {
+    console.log(stepper)
+    stepper.selectedIndex = index;
+  }
+}
